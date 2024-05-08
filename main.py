9bdcb6908465e3121a4ceec4b095a49a641b0e05
kaiobendrauf <38166195+kaiobendrauf@users.noreply.github.com>
2024-05-08 23:24:57 +0200

    rewrite json

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ import urllib.request
 CONFIG_FILE_NAME = "configuration.yml"
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 HEADERS = {'accept': 'application/json', 'User-Agent': 'nudge-auto-updater/1.0'}
+DEFAULT_CONFIG = 
+{
+	"targets" : [{"target":"default", "update_to":"latest"}]
+  "cve_urgency_conditions" : { "fraction_actively_exploited_CVEs" : 0.75 }
+  "default_deadline_days" : 14
+  "urgent_deadline_days" : 7
+}
+
+using_default_config = False
 
 try:
 	api_key = os.environ["NVD_API_KEY"]
@@ -18,7 +27,6 @@ try:
 	logging.info("Using the provided NVD API key...")
 except KeyError as e:
 	logging.warning(f"The {e} environment variable is not set. You will be rate-limited by the NIST NVD API.")
-	
 
 try:
 	import yaml
@@ -27,6 +35,7 @@ except ModuleNotFoundError as e:
 		logging.error(f"Can't read configuration file: {e}")
 		sys.exit(1)
 	else:
+		using_default_config = True
 		logging.warning("PyYAML library could not be loaded, but no configuration file is present.\nWill continue with default settings.")
 
 # ----------------------------------------
@@ -115,15 +124,15 @@ def get_nudge_config() -> dict:
 		f = open("nudge-config.json")
 		try:
 			data = json.load(f)
-		except Error as e:
+		except Exception as e:
 			logging.error("Unable to load nudge-config.json")
 			sys.exit(1)
-	except Error as e:
+	except Exception as e:
 		logging.error("Unable to open nudge-config.json")
 		sys.exit(1)
 
 	logging.info("Successfully loaded Nudge config!")
-	return read_nudge_requirements(data)
+	return data, read_nudge_requirements(data)
 
 def read_nudge_requirements(d:dict):
 	result = dict()
@@ -131,12 +140,35 @@ def read_nudge_requirements(d:dict):
 		target_str = "default"
 		if "targetedOSVersionsRule" in nudge_requirement:
 			target_str = nudge_requirement["targetedOSVersionsRule"]
-		result[target_str] = {"version":Version(nudge_requirement["requiredMinimumOSVersion"]),
-													"date":datetime.datetime.strptime(nudge_requirement["requiredInstallationDate"], DATE_FORMAT)}
+		result[target_str] = {"version":Version(nudge_requirement["requiredMinimumOSVersion"])}
 	return result
 
 def write_nudge_config(d:dict):
-	pass
+	try:
+		with open('nudge-config.json', 'w') as f:
+	    json.dump(d, f)
+	except Exception as e:
+		logging.error("Unable to write to nudge-config.json")
+		sys.exit(1)
+
+def adjust_url(url, change):
+	i = url.rfind("/") + 1
+	url = url[i]
+	url += change
+
+def update_nudge_file_dict(d:dict, target, version, url, days):
+	for i, requirement in iterate(d["osVersionRequirements"]):
+		if requirement["targetedOSVersionsRule"] == target:
+			d["osVersionRequirements"][i]["aboutUpdateURL_disabled"] = adjust_url(d["osVersionRequirements"]["aboutUpdateURL_disabled"], url)
+			d["osVersionRequirements"][i]["aboutUpdateURLs"]["aboutUpdateURL"] = adjust_url(d["osVersionRequirements"]["aboutUpdateURLs"]["aboutUpdateURL"], url)
+			date = datetime.datetime.strptime(requirement["requiredInstallationDate"], DATE_FORMAT)
+			date = date + datetime.timedelta(days=days)
+			datestr = date.strftime(DATE_FORMAT)
+			d["osVersionRequirements"][i]["requiredInstallationDate"] = datestr
+			d["osVersionRequirements"][i]["requiredMinimumOSVersion"] = str(version)
+			return d
+	logging.error(f"Unable to find target {target} in nudge-config.json.")
+	sys.exit(1)
 
 # ----------------------------------------
 # 								macOS
@@ -151,7 +183,7 @@ def get_macos_data():
 	try:
 		result = json.loads(response.read().decode('utf-8'))
 		logging.info("Successfully loaded macOS release data from SOFA!")
-	except Error as e:
+	except Exception as e:
 		logging.error("Unable to load macOS release data from SOFA.")
 		sys.exit(1)
 	return read_macos_data(result)
@@ -159,16 +191,18 @@ def get_macos_data():
 def read_macos_data(d:dict):
 	releases = []
 	cves = {}
+	urls = {}
 	for release in d["OSVersions"]:
 		version = Version(release["Latest"]["ProductVersion"])
 		releases.append(version)
 		found_security_release = False
 		for security_release in release["SecurityReleases"]:
 			if security_release["ProductVersion"] == release["Latest"]["ProductVersion"]:
+				urls[str(version)] = process_url(security_release["SecurityInfo"])
 				if "CVEs" in security_release:
 					security_release_cves = security_release["CVEs"]
 					for cve in security_release_cves:
-						security_release_cves[cve] = get_CVE_info(cve, security_release_cves[cve])
+						security_release_cves[cve] = {cve, security_release_cves[cve]}
 					cves[str(version)] = security_release_cves
 				else:
 					cves[str(version)] = dict()
@@ -177,9 +211,13 @@ def read_macos_data(d:dict):
 		if not found_security_release:
 			logging.error(f"Unable to find security release for macOS {version}")
 			sys.exit(1)
-	return releases, cves
+	return releases, cves, urls
 
-def get_CVE_info(s:str, b:bool):
+def process_url(s:str):
+	parts = s.split("/")
+	return parts[-1]
+
+def get_CVE_scores(s:str, b:bool):
 	req = urllib.request.Request(url=f"https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={s}", headers=HEADERS, method="GET")
 	try:
 		response = urllib.request.urlopen(req)
@@ -188,12 +226,15 @@ def get_CVE_info(s:str, b:bool):
 		sys.exit(1)
 	try:
 		result = json.loads(response.read().decode('utf-8'))
-	except Error as e:
+	except Exception as e:
 		logging.error(f"Unable to load CVE data for {s}.")
 		sys.exit(1)
-	return read_CVE_info(result["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0], b)
+	if "cvssMetricV31" in result["vulnerabilities"][0]["cve"]["metrics"]:
+		return read_CVE_scores(result["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV31"][0], b)
+	else:
+		return None
 
-def read_CVE_info(d:dict, b:bool):
+def read_CVE_scores(d:dict, b:bool):
 	print(d)
 	result = dict()
 	result["baseScore"] = d["cvssData"]["baseScore"]
@@ -206,12 +247,19 @@ def read_CVE_info(d:dict, b:bool):
 # 					  	Configurations
 # ----------------------------------------
 def get_config() -> dict:
-	result = [{"target":"default", "update_to":"latest"}]
+	if not os.path.exists(CONFIG_FILE_NAME):
+		using_default_config = True
+		logging.warning("No configuration file is present. Will continue with default settings.")
+	if using_default_config:
+		return DEFAULT_CONFIG
 	with open(CONFIG_FILE_NAME, "r") as config_yaml:
 		logging.info(f"Loading {CONFIG_FILE_NAME} ...")
 		try:
 			result = yaml.safe_load(config_yaml)
 			logging.info(f"Successfully loaded {CONFIG_FILE_NAME}!")
+			if result == None or len(result) < 1:
+				return DEFAULT_CONFIG 
+			return result
 		except yaml.YAMLError as e:
 			logging.error(f"Unable to load {CONFIG_FILE_NAME}")
 			sys.exit(1)
@@ -258,7 +306,7 @@ def read_formula(formula_str:str, cve_name:str, cve:dict):
 				temp_str = formula_str
 				formula_str = re.sub(r"[0-9]+(\.[0-9]+)?-[0-9]+(\.[0-9]+)?", sub_subformula, formula_str)
 		return float(formula_str)
-	except Error as e:
+	except Exception as e:
 		logging.error(f"Unable to interpret cve_urgency_conditions formula {s} for CVE {cve_name}.")
 		sys.exit(1)
 
@@ -302,7 +350,10 @@ def brackets_subformula(match):
 # ----------------------------------------
 # 				  Check CVE Conditions
 # ----------------------------------------
-def is_deadline_urgent(conditions, cves):
+def is_deadline_urgent(conditions, cves_scores, cves):
+	return check_cve_scores(conditions, cves_scores) or check_cve_scores(conditions, cves)
+
+def check_cve_scores(conditions, cves):
 	if len(cves) < 1:
 		return False
 	for score in ["baseScore", "exploitabilityScore", "impactScore"]:
@@ -321,56 +372,57 @@ def is_deadline_urgent(conditions, cves):
 			if (sum(l) / len(l)) > conditions[f"average_{score}"]:
 				logging.info(f'CVE urgency condition met! Average {score} of {(sum(l) / len(l))} is higher than threshhold {conditions[f"average_{score}"]}.')
 				return True
+	if "formulas" in conditions:
+		for formula in conditions["formulas"]:
+			l = []
+			for cve in cves:
+				l.append(read_formula(formula["formula"], cve, cves[cve]))
+			if formula["comparison"] == "average":
+				if (sum(l) / len(l)) > formula["threshhold"]:
+					logging.info(f'CVE urgency condition met! CVEs had an average score for formula {formula["formula"]} ({(sum(l) / len(l))}) higher than threshold {formula["threshhold"]}.')
+					return True
+			if formula["comparison"] == "max":
+				l.sort(reverse=True)
+				if l[0] > formula["threshhold"]:
+					logging.info(f'CVE urgency condition met! CVEs had an max score for formula {formula["formula"]} ({l[0]}) higher than threshold {formula["threshhold"]}.')
+					return True
+			if formula["comparison"] == "sum":
+				if sum(l) > formula["threshhold"]:
+					logging.info(f'CVE urgency condition met! CVEs had an summed score for formula {formula["formula"]} ({sum(l)}) higher than threshold {formula["threshhold"]}.')
+					return True
+			if formula["comparison"] == "n_above":
+				n = formula["n"]
+				if len(l) > n:
+					l.sort(reverse=True)
+					if l[n-1] > formula["threshhold"]:
+						logging.info(f'CVE urgency condition met! At least {n} CVEs had a score for formula {formula["formula"]} above the threshold {formula["threshhold"]}.')
+						return True
+	return False
+
+def check_cve_numbers(conditions, cves):
 	if "number_CVEs" in conditions:
 		if len(cves) < conditions["number_CVEs"]:
 			logging.info(f'CVE urgency condition met! Number of CVEs ({len(cves)}) is higher than threshhold {conditions["number_CVEs"]}.')
 			return True
 	if "number_actively_exploited_CVEs" in conditions:
-		l = []
-		for cve in cves:
-			l.append(cves[cve]["is_actively_exploited"])
-		if sum(l) > conditions[f"number_actively_exploited_CVEs"]:
+		if sum(cves.values()) > conditions[f"number_actively_exploited_CVEs"]:
 			logging.info(f'CVE urgency condition met! Number of actively exploited CVEs ({sum(l)}) is higher than threshhold {conditions["number_actively_exploited_CVEs"]}.')
 			return True
 	if "fraction_actively_exploited_CVEs" in conditions:
-		l = []
-		for cve in cves:
-			l.append(cves[cve]["is_actively_exploited"])
-		if (sum(l) / len(l)) > conditions["fraction_actively_exploited_CVEs"]:
-			logging.info(f'CVE urgency condition met! Fracction of actively exploited CVEs ({(sum(l) / len(l))}) is higher than threshold {conditions["fraction_actively_exploited_CVEs"]}.')
+		if (sum(cves.values()) / len(cves)) > conditions["fraction_actively_exploited_CVEs"]:
+			logging.info(f'CVE urgency condition met! Fraction of actively exploited CVEs ({(sum(l) / len(l))}) is higher than threshold {conditions["fraction_actively_exploited_CVEs"]}.')
 			return True
-	if "formulas" in conditions:
-		for formula in conditions["formulas"]:
-			pass
+	return False
 
-
-
-# cve_urgency_conditions:
-#   max_baseScore : 10
-#   average_baseSeverity : 8
-#   max_exploitabilityScore : 10
-#   average_exploitabilityScore : 8
-#   max_impactScore : 10
-#   average_impactScore : 8
-#   number_CVEs : 10
-#   number_actively_exploited_CVEs : 5
-#   fraction_actively_exploited_CVEs : 0.7
-#   formulas:
-#     - comparison : "average"
-#       formula : "baseScore * exploitabilityScore * impactScore"
-#       threshhold : 500
-#     - comparison : "max"
-#       formula : "baseScore * exploitabilityScore * impactScore * is_actively_exploited"
-#       threshhold : 200
-#     - comparison : "sum"
-#       formula    : "baseScore  * impactScore * is_actively_exploited"
-#       threshhold : 300
-
+# ----------------------------------------
+# 				  			Main
+# ----------------------------------------
 def main():
-	nudge_requirements = get_nudge_config()
-	latest_macos_releases, cves = get_macos_data()
+	nudge_file_dict, nudge_requirements = get_nudge_config()
+	latest_macos_releases, cves, urls = get_macos_data()
 	latest_macos_releases.sort(reverse=True)
 	config = get_config()
+	nudge_file_needs_updating = False
 
 	# check per configuration if it needs to be updates
 	for target in config["targets"]:
@@ -379,8 +431,11 @@ def main():
 			if target["update_to"] == "latest":
 				# nudge requirement needs to be checked against latest macOS
 				if nudge_requirements[target["target"]]["version"] < latest_macos_releases[0]:
+					is_uptodate = False
+					new_macos_release = latest_macos_releases[0]
 					logging.info(f"Nudge is old (nudge={nudge_requirements[target['target']]['version']}, newest={latest_macos_releases[0]})")
 				else:
+					is_uptodate = True
 					logging.info(f"Nudge configuration for target {target['target']} is already up to date.")
 			else:
 				# nudge requirement needs to be checked against latest macOS that is up to config macOS
@@ -390,26 +445,28 @@ def main():
 					if macos_release < config_version_gt and macos_release > nudge_requirements[target["target"]]["version"]:
 						logging.info(f"Nudge is old (nudge={nudge_requirements[target['target']]['version']}, newest={macos_release})")
 						is_uptodate = False
-				if is_uptodate:
-					logging.info(f"Nudge configuration for target {target['target']} is already up to date.")
-
-
-	# if nudge needs to be updated, ther we can assess the CVEs to determine the relevant deadline
-		# get all cves for the latest release
-		# is that cve being actively exploited/
-		# the severity rating of that cve
-	# then we need toupdate nudge
-
-
-	# test stuff
-	print(cves)
-	print(get_CVE_info("CVE-2024-1580"))
-
-
-	
-	
-
-
+						new_macos_release = macos_release
+						break
+			if is_uptodate:
+				logging.info(f"Nudge configuration for target {target['target']} is already up to date.")
+			else:
+				# nudge is not up to date! Is the new update urgent?
+				# get security metrics
+				security_release_cves_scores = dict()
+				security_release_cves = cves[macos_release]
+				for cve in security_release_cves:
+					cve_scores = get_CVE_scores(cve, security_release_cves[cve])
+					if cve_info:
+						security_release_cves_info[cve] = cve_info
+				if is_deadline_urgent(config["cve_urgency_conditions"], security_release_cves_scores, security_release_cves):
+					days = config["urgent_deadline_days"]
+				else:
+					days = config["default_deadline_days"]
+				nudge_file_dict = update_nudge_file_dict(nudge_file_dict, target["target"], new_macos_release, urls[new_macos_release], days)
+				nudge_file_needs_updating = True
+	# if nudge dict has changed rewrite it
+	if nudge_file_needs_updating:
+		write_nudge_config(nudge_file_dict)
 
 def setup_logging():
 	logger = logging.getLogger(__name__)
